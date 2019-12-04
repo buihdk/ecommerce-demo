61b84b4f60b2ccc20aeaed93b1fd30aa8ddb1f82
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-05 02:21:14 +0700

    transform js to jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import Item from './Item';
 
 import './App.css';
 
 const App = () => (
   <div className="App">
     <h1> Hello, World! </h1>
+    <Item />
   </div>
 );
 
---
+++ client/src/components/ListItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ListItem(props) {
+  return <div></div>;
+}
+
+ListItem.propTypes = {};
+
+export default ListItem;
