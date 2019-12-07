9f530cf98fd32f0d2f23c37d8442cb5e0f61a051
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-07 15:43:04 +0700

    update itemList and itemDetail

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -1,20 +1,108 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import {
+  makeStyles,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+} from '@material-ui/core';
+import { Favorite } from '@material-ui/icons';
 
-import ItemDetail from './ItemDetail';
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+    position: 'relative',
+  },
+  soldRibbon: {
+    position: 'absolute',
+    top: -16,
+    left: -82,
+    zIndex: 100,
+    backgroundColor: '#e43',
+    color: 'white',
+    width: 200,
+    lineHeight: '26px',
+    paddingTop: 36,
+    textAlign: 'center',
+    letterSpacing: 3,
+    transform: 'rotate(-45deg)',
+  },
+  media: {
+    height: 140,
+  },
+});
 
-const ItemList = ({ items }) => (
-  <div>
-    {items && items.map(item => <ItemDetail key={item.id} item={item} />)}
-  </div>
-);
+const ItemList = ({ items }) => {
+  const styles = useStyles();
+
+  return (
+    <div>
+      {items &&
+        items.map(item => (
+          <Card key={item.id} className={styles.card}>
+            {item.is_sold_out && (
+              <span className={styles.soldRibbon}>SOLD</span>
+            )}
+            <CardActionArea>
+              <CardMedia
+                className={styles.media}
+                image={item.image}
+                title={item.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {item.description}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {`¥ ${item.price}`}
+                </Typography>
+                <Favorite fontSize="small" />
+                {item.like_count}
+              </CardContent>
+              <Link
+                to={{
+                  pathname: `items/${item.id}`,
+                }}
+              >
+                <Button size="small">Learn More</Button>
+              </Link>
+            </CardActionArea>
+          </Card>
+        ))}
+    </div>
+  );
+};
 
 ItemList.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.object),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      is_sold_out: PropTypes.bool,
+      image: PropTypes.string,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      price: PropTypes.number,
+      like_count: PropTypes.number,
+    }),
+  ),
 };
 
 ItemList.defaultProps = {
-  items: [],
+  items: [
+    {
+      id: '',
+      is_sold_out: false,
+      image: '',
+      name: '',
+      description: '',
+      price: 0,
+      like_count: 0,
+    },
+  ],
 };
 
 export default memo(ItemList);
