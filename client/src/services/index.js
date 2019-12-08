e2f0447005ce19cfb4e6012d3c0225ac0740a49f
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-08 22:51:54 +0700

    update err msgs

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -5,20 +5,20 @@ export default {
     fetch(`${apiServerUrl}/items`)
       .then(res => res.json())
       .catch(err => ({
-        err: `Fetch problem: ${err}. Please check console and API server.`,
+        err: `Fetch problem: ${err} items. Please check console and API server.`,
       })),
 
   fetchItemById: id =>
     fetch(`${apiServerUrl}/items/${id}`)
       .then(res => res.json())
       .catch(err => ({
-        err: `Fetch problem: ${err}. Please check console and API server.`,
+        err: `Fetch problem: ${err} item by ID. Please check console and API server.`,
       })),
 
   fetchCategories: () =>
     fetch(`${apiServerUrl}/categories`)
       .then(res => res.json())
       .catch(err => ({
-        err: `Fetch problem: ${err}. Please check console and API server.`,
+        err: `Fetch problem: ${err} categories. Please check console and API server.`,
       })),
 };
