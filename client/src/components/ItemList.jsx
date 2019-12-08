f7cab08cc037f1761b1fc78db6b5aa4d6bb1da86
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-08 13:52:43 +0700

    update View button to see Details page

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -1,19 +1,19 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
 import {
   makeStyles,
   Box,
   Grid,
   Card,
-  CardMedia,
   CardContent,
   Typography,
-  Button,
+  ButtonBase,
 } from '@material-ui/core';
 import { FavoriteBorder } from '@material-ui/icons';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   card: {
     position: 'relative',
   },
@@ -31,18 +31,80 @@ const useStyles = makeStyles({
     letterSpacing: 3,
     transform: 'rotate(-45deg)',
   },
-  media: {
-    height: 400,
-    width: 400,
-  },
   icon: {
-    fontSize: 13,
-    marginRight: 2,
+    fontSize: 17,
+    marginRight: 4,
   },
-});
+  image: {
+    position: 'relative',
+    height: 400,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15,
+      },
+      '& $imageMarked': {
+        opacity: 0,
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor',
+      },
+    },
+  },
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
+      6}px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  },
+}));
 
 const ItemList = ({ items }) => {
   const styles = useStyles();
+  const history = useHistory();
 
   return (
     <Grid container justify="center" spacing={2}>
@@ -53,9 +115,33 @@ const ItemList = ({ items }) => {
               {item.is_sold_out && (
                 <Box className={styles.soldRibbon}>SOLD</Box>
               )}
-              <CardMedia className={styles.media} image={item.image} />
+              <ButtonBase
+                focusRipple
+                key={item.name}
+                className={styles.image}
+                focusVisibleClassName={styles.focusVisible}
+                style={{ width: 400 }}
+                onClick={() => history.push(`items/${item.id}`)}
+              >
+                <span
+                  className={styles.imageSrc}
+                  style={{ backgroundImage: `url(${item.image})` }}
+                />
+                <span className={styles.imageBackdrop} />
+                <span className={styles.imageButton}>
+                  <Typography
+                    component="span"
+                    variant="subtitle1"
+                    color="inherit"
+                    className={styles.imageTitle}
+                  >
+                    View
+                    <span className={styles.imageMarked} />
+                  </Typography>
+                </span>
+              </ButtonBase>
               <CardContent>
-                <Typography variant="body1" color="textSecondary">
+                <Typography variant="h6" color="textSecondary">
                   {item.name}
                   <Box display="flex" justifyContent="space-between">
                     <Typography variant="h6" color="textPrimary">
@@ -67,11 +153,6 @@ const ItemList = ({ items }) => {
                     </Box>
                   </Box>
                 </Typography>
-                <Link to={{ pathname: `items/${item.id}` }}>
-                  <Button variant="outline" size="small">
-                    View Details
-                  </Button>
-                </Link>
               </CardContent>
             </Card>
           </Grid>
