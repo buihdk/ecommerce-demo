282a3dc79729de1fa765dbdd682938ad3d6fba92
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-08 15:42:06 +0700

    rename server url

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,18 +1,18 @@
-const url = 'http://localhost:5000';
+const apiServerUrl = 'http://localhost:5000';
 
 export default {
   fetchItems: () =>
-    fetch(`${url}/items`)
+    fetch(`${apiServerUrl}/items`)
       .then(res => res.json())
       .catch(err => console.log(`Fetch problem: ${err}`)),
 
   fetchItemById: id =>
-    fetch(`${url}/items/${id}`)
+    fetch(`${apiServerUrl}/items/${id}`)
       .then(res => res.json())
       .catch(err => console.log(`Fetch problem: ${err}`)),
 
   fetchCategories: () =>
-    fetch(`${url}/categories`)
+    fetch(`${apiServerUrl}/categories`)
       .then(res => res.json())
       .catch(err => console.log(`Fetch problem: ${err}`)),
 };
