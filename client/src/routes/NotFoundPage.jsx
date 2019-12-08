92ab67510d2b8a494c87720118632ede64580ec1
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-08 16:38:43 +0700

    update NotFound page to go back to home

--- client/src/routes/NotFoundPage.jsx
+++ client/src/routes/NotFoundPage.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import React, { memo } from 'react';
+import { useHistory } from 'react-router-dom';
+import { makeStyles, Box, Typography, Button } from '@material-ui/core';
 
-function NotFoundPage() {
+const useStyles = makeStyles({
+  container: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emoji: {
+    fontSize: 40,
+    letterSpacing: 10,
+  },
+});
+
+const NotFoundPage = () => {
+  const styles = useStyles();
+  const history = useHistory();
+  const handleClick = () => history.push('/');
+
   return (
-    <Box display="flex" justifyContent="center" flexDirection="column">
+    <Box className={styles.container}>
       <Typography variant="h1">Error 404</Typography>
-      <Typography variant="h6">Page not found.</Typography>
-      😵😖😫
+      <Typography variant="h6" gutterBottom>
+        Page not found.
+      </Typography>
+      <span className={styles.emoji} role="img" aria-label="sad-faces">
+        😵😖😫
+      </span>
+      <Button onClick={handleClick} variant="outlined">
+        Go back to homepage
+      </Button>
     </Box>
   );
-}
+};
 
-export default NotFoundPage;
+export default memo(NotFoundPage);
