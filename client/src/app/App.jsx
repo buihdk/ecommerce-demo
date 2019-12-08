99985ae24ea3582b07c473d11e4da08f4f411b0b
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-08 23:35:36 +0700

    fix module not found

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import { Container } from '@material-ui/core';
 
-import { fetchCategories } from './services/localStorage';
+import { fetchCategories } from '../services/localStorage';
 import Routes from '../routes';
 
 fetchCategories();
