49c62f37b61099455504b3652f82e95a8514b669
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-09 19:13:18 +0700

    fix wrapped into act(...) warning at routes

--- client/src/routes/index.test.jsx
+++ client/src/routes/index.test.jsx
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react';
+import { act } from 'react-dom/test-utils';
 import { MemoryRouter } from 'react-router';
 import { mount } from 'enzyme';
 import toJSON from 'enzyme-to-json';
@@ -17,38 +18,46 @@ jest.spyOn(require('../services'), 'fetchItemById').mockResolvedValue(
 );
 
 describe('Routes', () => {
-  test('renders ItemListPage without crashing', () => {
-    const wrapper = mount(
-      <MemoryRouter initialEntries={['/']}>
-        <Suspense fallback={<div>loading...</div>}>
-          <Routes />
-        </Suspense>
-      </MemoryRouter>,
-    );
+  let wrapper;
+
+  test('renders ItemListPage without crashing', async () => {
+    await act(async () => {
+      wrapper = await mount(
+        <MemoryRouter initialEntries={['/']}>
+          <Suspense fallback={<div>loading...</div>}>
+            <Routes />
+          </Suspense>
+        </MemoryRouter>,
+      );
+    });
 
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
-  test('renders ItemDetailPage without crashing', () => {
-    const wrapper = mount(
-      <MemoryRouter initialEntries={['/items/1']}>
-        <Suspense fallback={<div>loading...</div>}>
-          <Routes />
-        </Suspense>
-      </MemoryRouter>,
-    );
+  test('renders ItemDetailPage without crashing', async () => {
+    await act(async () => {
+      wrapper = await mount(
+        <MemoryRouter initialEntries={['/items/1']}>
+          <Suspense fallback={<div>loading...</div>}>
+            <Routes />
+          </Suspense>
+        </MemoryRouter>,
+      );
+    });
 
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
-  test('renders NotFoundPage without crashing', () => {
-    const wrapper = mount(
-      <MemoryRouter initialEntries={['/404']}>
-        <Suspense fallback={<div>loading...</div>}>
-          <Routes />
-        </Suspense>
-      </MemoryRouter>,
-    );
+  test('renders NotFoundPage without crashing', async () => {
+    await act(async () => {
+      wrapper = await mount(
+        <MemoryRouter initialEntries={['/404']}>
+          <Suspense fallback={<div>loading...</div>}>
+            <Routes />
+          </Suspense>
+        </MemoryRouter>,
+      );
+    });
 
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
