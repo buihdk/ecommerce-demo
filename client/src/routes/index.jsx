ad1211e2c4dccc3fc6242cf8073716ac23b38763
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-09 16:02:10 +0700

    add unit tests for routes

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,40 +1,26 @@
-import React, { Suspense, lazy } from 'react';
+import React, { memo, lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { CircularProgress } from '@material-ui/core';
-
-import ErrorBoundary from '../utils/ErrorBoundary';
-import ErrorBar from '../utils/ErrorBar';
 
 const ItemListPage = lazy(() => import('../pages/ItemListPage'));
 const ItemDetailPage = lazy(() => import('../pages/ItemDetailPage'));
 const NotFoundPage = lazy(() => import('../utils/NotFoundPage'));
 
 const Routes = () => (
-  <ErrorBoundary
-    fallback={<ErrorBar msg="App has crashed. Check console for more info." />}
-  >
-    <Suspense fallback={<CircularProgress />}>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          component={props => <ItemListPage {...props} />}
-        />
-        <Route
-          exact
-          path="/items/:id"
-          component={props => <ItemDetailPage {...props} />}
-        />
-        <Route exact path="/items">
-          <Redirect to="/" />
-        </Route>
-        <Route
-          path="*"
-          component={props => <NotFoundPage errorCode={404} {...props} />}
-        />
-      </Switch>
-    </Suspense>
-  </ErrorBoundary>
+  <Switch>
+    <Route exact path="/" component={props => <ItemListPage {...props} />} />
+    <Route
+      exact
+      path="/items/:id"
+      component={props => <ItemDetailPage {...props} />}
+    />
+    <Route exact path="/items">
+      <Redirect to="/" />
+    </Route>
+    <Route
+      path="*"
+      component={props => <NotFoundPage errorCode={404} {...props} />}
+    />
+  </Switch>
 );
 
-export default Routes;
+export default memo(Routes);
---
+++ client/src/routes/index.test.jsx
@@ -0,0 +1,57 @@
+import React, { Suspense } from 'react';
+import { MemoryRouter } from 'react-router';
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+
+import Routes from '.';
+
+jest.spyOn(require('../services'), 'fetchItems').mockImplementation(
+  () =>
+    new Promise(resolve => {
+      resolve({ data: [] });
+    }),
+);
+jest.spyOn(require('../services'), 'fetchItemById').mockImplementation(
+  () =>
+    new Promise(resolve => {
+      resolve({ category_id: 1 });
+    }),
+);
+
+describe('Routes', () => {
+  test('renders ItemListPage without crashing', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/']}>
+        <Suspense fallback={<div>loading...</div>}>
+          <Routes />
+        </Suspense>
+      </MemoryRouter>,
+    );
+
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  test('renders ItemDetailPage without crashing', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/items/1']}>
+        <Suspense fallback={<div>loading...</div>}>
+          <Routes />
+        </Suspense>
+      </MemoryRouter>,
+    );
+
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  test('renders NotFoundPage without crashing', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/404']}>
+        <Suspense fallback={<div>loading...</div>}>
+          <Routes />
+        </Suspense>
+      </MemoryRouter>,
+    );
+
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+});
