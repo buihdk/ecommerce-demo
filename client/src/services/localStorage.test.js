a2b3aefa2383d4e984d89ad3145a26b3f692cd2a
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2019-12-09 17:56:01 +0700

    update tests

--- client/src/services/localStorage.test.js
+++ client/src/services/localStorage.test.js
@@ -1,11 +1,11 @@
 import { CATEGORIES, fetchCategories, getCategories } from './localStorage';
 
-const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
-
 describe('localStorage', () => {
   describe('getCategories', () => {
     test('fetchCategories should work', done => {
+      const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
       fetchCategories();
+
       setTimeout(() => {
         expect(setItemSpy).toBeCalledWith(CATEGORIES, '[]');
         done();
@@ -16,12 +16,14 @@ describe('localStorage', () => {
   describe('getCategories', () => {
     test('when JSON.parse returns null', () => {
       JSON.parse = jest.fn().mockResolvedValueOnce(null);
+
       getCategories().then(data => expect(data).toEqual(null));
     });
 
     test('getCategories is called with CATEGORIES', () => {
       const getItemSpy = jest.spyOn(Storage.prototype, 'getItem');
       getCategories();
+
       expect(getItemSpy).toBeCalledWith(CATEGORIES);
     });
   });
